ed82f0f20234d0a9c347c26fd7952f7e201cd71c
Sanan Yuzb <y.sanan95@gmail.com>
2015-10-13 20:17:29 +0500

    First version

---
+++ src/com/company/Main.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    static int k,n,two,one;
+
+    static Scanner scanner = new Scanner(System.in);
+
+    static long l;
+
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i < 200; i++) {
+            System.out.println(i);
+        }
+
+    }
+
+    static int gcd(int a, int b)
+    {
+        return (b == 0) ? a : gcd(b, a%b);
+    }
+
+    static int nok(int a, int b)
+    {
+        return a*b/gcd(a, b);
+    }
+
+    static int max(int [] a,int i) {
+
+        int maxx = Integer.MIN_VALUE;
+
+        for (int j = i + 1; j < a.length; j++) {
+
+            if (a[j] > maxx) {
+
+                maxx = a[j];
+
+            }
+
+        }
+
+
+
+        return maxx;
+    }
+
+
+}
